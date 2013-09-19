6b9b4807cb9c86af2578f5786595e03115d76ded
Quentin Roy <quentin.roy@ge.com>
2013-09-19 18:14:58 +0200

    Magic setter (path analysis) in the trial-logger.js

--- js/xpclient/trials/trial-logger.js
+++ js/xpclient/trials/trial-logger.js
@@ -2,7 +2,7 @@
 /*global define */
 
 
-define(['./trial-logger-processors', 'jstools/tools', 'classy', 'jquery'], function (processors, tools, Class, $) {
+define(['jstools/tools', 'classy', 'jquery'], function (tools, Class, $) {
     "use strict";
 
 
@@ -13,51 +13,80 @@ define(['./trial-logger-processors', 'jstools/tools', 'classy', 'jquery'], funct
 
             this._events = [];
 
-            if (typeof processor_s == "Array") {
-                this.processors = processor_s.slice(0);
-            } else if (typeof processor_s == "Function") {
+            if (typeof processor_s == "function") {
                 this.processors = [processor_s];
+            } else if (processor_s){
+                this.processors = processor_s.slice(0);
             } else {
                 this.processors = [];
-                for (var proc in processors) {
-                    this.processors.push(processors[proc]);
-                }
             }
 
             this.trialParams = trialParams;
         },
 
         trialTime: function () {
-            return this._log.trialStartTimestamp - new Date().getTime();
+            return this._log.timestamps.trialStart - new Date().getTime();
         },
-
-        trialStart: function (startTime) {
-            if (this._log.trialStartTimestamp) throw "Already started.";
-            this._log.trialStartTimestamp = startTime || new Date().getTime();
-        },
-
-        trialEnd: function (endTime) {
-            if (!this._log.trialStartTimestamp) throw "Not started.";
-            if (this._log.trialEndTimestamp) throw "Alreaded ended.";
-            endTime = endTime || new Date().getTime();
-            if (endTime < this._log.trialStartTimestamp) throw "End time lesser than start time";
-            this._log.trialEndTimestamp = endTime;
-        },
+        
 
         timestamp: function (measureName) {
-            if (!typeof measureName === "string") throw "measureName must be a string";
+            if (typeof measureName !== 'string') throw "measureName must be a string";
             this.set(measureName, new Date().getTime());
         },
 
-        set: function (measuresOrMeasureName, measure) {
-            if (typeof measuresOrMeasureName === "string") {
-                this._log[measuresOrMeasureName] = measure;
+
+        _makePaths: function (objPath, prefix) {
+            var paths = [],
+                subsubPaths, subPath;
+            prefix = tools.isSet(prefix) ? prefix : '';
+            if (typeof objPath === 'string') {
+                paths.append(prefix + ':' + objPath);
             } else {
-                for (measure in measuresOrMeasureName) {
-                    this._log[measure] = measuresOrMeasureName[measure];
+                for (subPath in objPath) {
+                    subsubPaths = this._makePaths(objPath, prefix + '.' + subPath);
+                    paths = paths.concat(subsubPaths);
                 }
             }
-            return this;
+            return paths;
+        },
+
+        // because we love magic
+        _resolvePath: function (strPath, val) {
+            var objPath = {},
+                last = objPath,
+                lastName = null,
+                splitName = strPath.split('.'),
+                pathI;
+            for (pathI in splitName) {
+                if (lastName) {
+                    last = last[lastName] = {};
+                }
+                lastName = splitName[pathI];
+            }
+            last[lastName] = val;
+            return objPath;
+        },
+        
+        // because we love magic
+        _resolvePaths: function (obj) {
+            var propPath, propVal, subObj, newObj = {};
+            for (propPath in obj) {
+                propVal = obj[propPath];
+                propVal = propVal instanceof Object ? this._resolvePaths(propVal) : propVal;
+                subObj = this._resolvePath(propPath, propVal);
+                $.extend(true, newObj, subObj);
+            }
+            return newObj;
+        },
+
+        set: function (measureNameOrMeasuresObj, measure) {
+            var objPath;
+            if (typeof measureNameOrMeasuresObj === 'string') {
+                objPath = this._resolvePath(measureNameOrMeasuresObj, measure);
+            } else {
+                objPath = this._resolvePaths(measureNameOrMeasuresObj);
+            }
+            $.extend(true, this._log, objPath);
         },
 
         addEvent: function (event) {
