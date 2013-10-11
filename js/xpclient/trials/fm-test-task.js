529f55b658950bf0c6f24cd90733ed3a71de3fc2
Quentin Roy <quentin.roy@ge.com>
2013-10-11 19:32:50 +0200

    Finger Menu task logging. Update of SigmaMenu package.

--- js/xpclient/trials/fm-test-task.js
+++ js/xpclient/trials/fm-test-task.js
@@ -4,22 +4,29 @@ define(['fingermenu', './test-task', 'jquery'], function (FingerMenu, TestTask, 
 
     return TestTask.$extend({
 
-        _initTechnique: function (techniqueDiv) {
+        __init__: function () {
+            this.$super.apply(this, arguments);
+
+            // initialize stuffs
+            this._lastPos = null;
+            this._targetFinger = null;
+            this._targetLevel = null;
 
             // create the mappings
             this._fingerModes = this._createFingerModes();
             this._fingerLabels = this._createFingerLabels();
+        },
+
+
+        _initTechnique: function (techniqueDiv) {
 
             // create the menu
-            this._fingerMenu = new FingerMenu(this._fingerLabels, techniqueDiv, true);
+            this._fingerMenu = new FingerMenu(this._fingerLabels, techniqueDiv, true, this._logger);
 
             // add the handlers
             this._fingerMenu.fingerTouched.add($.proxy(this._onFingerTouched, this));
             this._fingerMenu.fingerReleased.add($.proxy(this._onFingerReleased, this));
             this._fingerMenu.fingerMoved.add($.proxy(this._onFingerMoved, this));
-
-            // initialize stuffs
-            this._lastPos = null;
         },
 
         _createFingerModes: function () {
@@ -34,11 +41,18 @@ define(['fingermenu', './test-task', 'jquery'], function (FingerMenu, TestTask, 
                 if (fNum === 0)
                     modes.push({});
                 modes[lvl][fingerName] = mode;
+
+                if (modeId === this._targetMode) {
+                    this._targetFinger = fingerName;
+                    this._targetLevel = lvl;
+                }
+
                 fNum++;
                 if (fNum >= fingerNames.length) {
                     fNum = 0;
                     lvl++;
                 }
+
             }
             return modes;
         },
@@ -58,13 +72,35 @@ define(['fingermenu', './test-task', 'jquery'], function (FingerMenu, TestTask, 
         },
 
         _onFingerTouched: function (finger) {
+            var targetPosition = this._fingerMenu.fingerPosition(this._targetLevel, this._targetFinger);
+            var selectedPosition = this._fingerMenu.fingerPosition(finger.level, finger.name);
+
+            this._logger.set({
+                fm: {
+                    selectedFinger: {
+                        name: finger.name,
+                        level: finger.level,
+                        x: selectedPosition[0],
+                        y: selectedPosition[1]
+                    },
+                    targetFinger: {
+                        name: this._targetFinger,
+                        level: this._targetLevel,
+                        x: targetPosition[0],
+                        y: targetPosition[1]
+                    }
+                },
+                triggerPos: {
+                    x: finger.pos[0],
+                    y: finger.pos[1]
+                }
+            });
             var modeId = this._fingerModes[finger.level][finger.name].id;
             this._modeSelected(modeId);
             this._lastPos = finger.pos;
         },
 
         _onFingerMoved: function (finger) {
-            console.log("moved");
             var dPos;
             if (this._lastPos) {
                 dPos = [finger.pos[0] - this._lastPos[0], finger.pos[1] - this._lastPos[1]];
