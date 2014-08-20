6c637769b36da4adff1fe75ac78fd406a6b73c81
Quentin Roy <quentin.roy@ge.com>
2014-08-20 03:28:34 +0200

    Add a "time for a break?" message.

--- views/block-init.js
+++ views/block-init.js
@@ -1,5 +1,5 @@
 define(['jquery', 'jstools/tools', 'text!./block-init-template.html', 'handlebars', 'fastclick', 'css!./main.css'], function ($, tools, template, Handlebars, FastClick) {
-    "use strict";
+    'use strict';
 
     function BlockInitView(parentView) {
         this._parentView = $(parentView);
@@ -10,18 +10,27 @@ define(['jquery', 'jstools/tools', 'text!./block-init-template.html', 'handlebar
 
         blockInit: function (blockinfo) {
 
-            var dfd = $.Deferred(),
-                compiledTemplate = this._template({
-                    name: blockinfo.practice ? "Practice" : "Block " + (blockinfo.measure_block_number + 1),
+            var dfd = $.Deferred();
+            
+            // compile the template
+            var compiledTemplate = this._template({
+                    first : blockinfo.number == 1,
+                    name  : blockinfo.practice ? 'Practice' : 'Block ' + (blockinfo.measure_block_number + 1),
                     values: blockinfo.values,
                     backgroundColor: blockinfo.practice ? '#025600' : '#563200'
                 });
             var div = $(compiledTemplate);
+            // append it to the parent view
+            div.appendTo(this._parentView);
+            // remove click mobile delay
             FastClick.attach(div[0]);
-            this._parentView.append(div);
+            // prevent any default events
+            tools.preventDefaultOnTouch(div);
+            // resolve on click
             div.click(function () {
                 dfd.resolve();
             });
+            // return
             return dfd.done(function () {
                 div.remove();
             });
