8b24d0bb265f99d5e36f2244d687efd5fd443cc7
Quentin Roy <quentin@quentinroy.fr>
2017-06-16 18:15:21 +0800

    Rollup builder.

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import pug from 'rollup-plugin-pug';
+import babel from 'rollup-plugin-babel';
+import json from 'rollup-plugin-json';
+
+export default {
+  entry: 'src/index.js',
+  format: 'umd',
+  moduleName: 'XpClient',
+  plugins: [resolve(), commonjs(), pug(), json(), babel({
+    exclude: 'node_modules/**' // only transpile our source code
+  })],
+  external: ['pug', 'fetch-ponyfill', 'javascript-state-machine'],
+  dest: 'lib/xpclient.js',
+  sourceMap: true,
+  globals: {
+    'javascript-state-machine': 'StateMachine',
+    'fetch-ponyfill': 'fetchPonyfill'
+  }
+};
