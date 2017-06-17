93a48609297431eb281e4a4082819e23458069cb
Quentin Roy <quentin@quentinroy.fr>
2017-06-17 15:34:40 +0800

    Remove useless rollup-plugin-json.

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,12 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import pug from 'rollup-plugin-pug';
 import babel from 'rollup-plugin-babel';
-import json from 'rollup-plugin-json';
 
 export default {
   entry: 'src/index.js',
   format: 'umd',
   moduleName: 'XpClient',
-  plugins: [resolve(), commonjs(), pug(), json(), babel({
+  plugins: [resolve(), commonjs(), pug(), babel({
     exclude: 'node_modules/**' // only transpile our source code
   })],
   external: ['pug', 'fetch-ponyfill', 'javascript-state-machine'],
