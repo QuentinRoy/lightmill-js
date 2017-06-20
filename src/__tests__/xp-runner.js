34d268e456c0a4f978d687d8c3888ffe7afe2d76
Quentin Roy <quentin@quentinroy.fr>
2017-06-20 08:57:07 +0800

    runTrials test now also monitor promise resolution time.

--- src/__tests__/xp-runner.js
+++ src/__tests__/xp-runner.js
@@ -17,19 +17,22 @@ test('runTrials works as expected', async t => {
   // Will register every spy call as { type, args }
   const mainSpy = spy();
   // Create a sub spy with the provided type.
-  const createSubSpy = (type, f) =>
+  const createSubSpy = (type, f, done = true) =>
     spy((...args) => {
-      mainSpy({ type, args });
-      return wait().then(f && (() => f(...args)));
+      mainSpy(type, args);
+      return wait().then(f && (() => f(...args))).then(r => {
+        if (done) mainSpy(`* ${type}`);
+        return r;
+      });
     });
   // Connection fake implementation.
   const trials = makeTrialList(2, 2);
   const connection = {
-    endCurrentTrial: createSubSpy('endCurrentTrial'),
+    endCurrentTrial: createSubSpy('endCurrentTrial', null, false),
     getCurrentTrial: createSubSpy('getCurrentTrial', () => trials.shift()),
     flush: createSubSpy('flush')
   };
-  // App fake implementation.
+  // App fake implementation,
   const app = {
     initBlock: createSubSpy('initBlock'),
     runTrial: createSubSpy('runTrial', trial => ({
@@ -41,30 +44,46 @@ test('runTrials works as expected', async t => {
   // Expected calls on connection and app methods.
   const expectedCalls = [
     // block 1 trial 1
-    [{ type: 'getCurrentTrial', args: [] }],
-    [{ type: 'initBlock', args: [{ number: 0 }] }],
-    [{ type: 'flush', args: [7] }],
-    [{ type: 'runTrial', args: [{ number: 0, block: { number: 0 } }] }],
-    [{ type: 'endCurrentTrial', args: [{ val: '0-0' }] }],
+    ['getCurrentTrial', []],
+    ['* getCurrentTrial'],
+    ['initBlock', [{ number: 0 }]],
+    ['* initBlock'],
+    ['flush', [7]],
+    ['* flush'],
+    ['runTrial', [{ number: 0, block: { number: 0 } }]],
+    ['* runTrial'],
+    ['endCurrentTrial', [{ val: '0-0' }]],
     // block 1 trial 2
-    [{ type: 'getCurrentTrial', args: [] }],
-    [{ type: 'flush', args: [7] }],
-    [{ type: 'runTrial', args: [{ number: 1, block: { number: 0 } }] }],
-    [{ type: 'endCurrentTrial', args: [{ val: '0-1' }] }],
+    ['getCurrentTrial', []],
+    ['* getCurrentTrial'],
+    ['flush', [7]],
+    ['* flush'],
+    ['runTrial', [{ number: 1, block: { number: 0 } }]],
+    ['* runTrial'],
+    ['endCurrentTrial', [{ val: '0-1' }]],
     // block 2 trial 1
-    [{ type: 'getCurrentTrial', args: [] }],
-    [{ type: 'initBlock', args: [{ number: 1 }] }],
-    [{ type: 'flush', args: [7] }],
-    [{ type: 'runTrial', args: [{ number: 0, block: { number: 1 } }] }],
-    [{ type: 'endCurrentTrial', args: [{ val: '1-0' }] }],
+    ['getCurrentTrial', []],
+    ['* getCurrentTrial'],
+    ['initBlock', [{ number: 1 }]],
+    ['* initBlock'],
+    ['flush', [7]],
+    ['* flush'],
+    ['runTrial', [{ number: 0, block: { number: 1 } }]],
+    ['* runTrial'],
+    ['endCurrentTrial', [{ val: '1-0' }]],
     // block 2 trial 2
-    [{ type: 'getCurrentTrial', args: [] }],
-    [{ type: 'flush', args: [7] }],
-    [{ type: 'runTrial', args: [{ number: 1, block: { number: 1 } }] }],
-    [{ type: 'endCurrentTrial', args: [{ val: '1-1' }] }],
+    ['getCurrentTrial', []],
+    ['* getCurrentTrial'],
+    ['flush', [7]],
+    ['* flush'],
+    ['runTrial', [{ number: 1, block: { number: 1 } }]],
+    ['* runTrial'],
+    ['endCurrentTrial', [{ val: '1-1' }]],
     // End
-    [{ type: 'getCurrentTrial', args: [] }],
-    [{ type: 'flush', args: [] }]
+    ['getCurrentTrial', []],
+    ['* getCurrentTrial'],
+    ['flush', []],
+    ['* flush']
   ];
   // Loop because that is easier to debug.
   expectedCalls.forEach((arg, i) => {
