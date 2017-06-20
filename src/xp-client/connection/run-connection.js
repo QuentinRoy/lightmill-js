9e90ea83154d057413b69a730d6cf70885754342
Quentin Roy <quentin@quentinroy.fr>
2017-06-20 23:46:52 +0800

    Fix experiment importation.

--- src/xp-client/connection/run-connection.js
+++ src/xp-client/connection/run-connection.js
@@ -241,8 +241,8 @@ RunConnection.create = async function createRunConnection(
     : serverAddressOrInterface;
 
   // Check if the experiment is loaded on the server, and if not load it.
-  if (await isExperimentLoadedOnServer(serverInterface, experimentId)) {
-    await importExperimentOnServer(serverInterface, experimentId);
+  if (!await isExperimentLoadedOnServer(serverInterface, experimentId)) {
+    await importExperimentOnServer(serverInterface, experimentDesignAddr);
   }
 
   // Connect to the run.
