eb4cea5adabe50b567c5b140362c6b1b091b3486
Quentin Roy <quentin@quentinroy.fr>
2018-08-16 15:57:42 -0400

    feat(runner): new interface for the stores
    BREAKING CHANGE: Stores do not need to implement `log` anymore but should implement `getLogger(logType)` that returns the corresponding logger instead.

--- packages/runner/esm/runner.js
+++ packages/runner/esm/runner.js
@@ -1,16 +1,18 @@
 import { reduceAsyncIterator } from './utils';
 
 export default ({ store, taskIterator, taskManager }) => ({
-  start: () =>
-    reduceAsyncIterator(taskIterator, async (_, task) => {
+  start: () => {
+    const taskLogger = store.getLogger('task');
+    return reduceAsyncIterator(taskIterator, async (_, task) => {
       // Run the task.
       const measures = await taskManager(task);
       // Log the results.
-      await store.log(task.id, { task, measures });
+      await taskLogger.log(task.id, { task, measures });
     }).then(async () => {
       // At the very end (when all tasks has been done), we mark the experiment
       // as complete. Do not directly return store.complete because we don't
       // want to return the result of the call.
       await store.complete();
-    })
+    });
+  }
 });
