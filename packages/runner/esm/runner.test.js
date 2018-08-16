f64e61f8c74b2f0f6fc14d8cdca46cbd42dae141
Quentin Roy <quentin@quentinroy.fr>
2018-08-16 16:34:56 -0400

    fix(runner): fix first task always being skipped

--- packages/runner/esm/runner.test.js
+++ packages/runner/esm/runner.test.js
@@ -14,28 +14,28 @@ describe('Runner#start', () => {
   beforeEach(() => {
     // i is used to make sure all functions are called in the right order,
     // and wait for each others.
-    let i = -1;
-    const getCallNb = () => {
-      i += 1;
-      return i;
+    let callIndex = -1;
+    const getCallIndex = () => {
+      callIndex += 1;
+      return callIndex;
     };
     genTasks = jest.fn(async function* mockGenTasks() {
       await wait(0);
-      yield { id: 'task1', i: getCallNb() };
+      yield { id: 'task1', callIndex: getCallIndex() };
       await wait(0);
-      yield { id: 'task2', i: getCallNb() };
+      yield { id: 'task2', callIndex: getCallIndex() };
       await wait(0);
-      yield { id: 'task3', i: getCallNb() };
+      yield { id: 'task3', callIndex: getCallIndex() };
       await wait(0);
     });
     taskManager = jest.fn(async task => {
       await wait(0);
-      return { result: `result-${task.id}`, i: getCallNb() };
+      return { result: `result-${task.id}`, callIndex: getCallIndex() };
     });
     taskLogger = {
       log: jest.fn(async () => {
         await wait(0);
-        return { i: getCallNb() };
+        return { callIndex: getCallIndex() };
       })
     };
     store = {
@@ -44,7 +44,7 @@ describe('Runner#start', () => {
       ),
       complete: jest.fn(async () => {
         await wait(0);
-        return { i: getCallNb() };
+        return { callIndex: getCallIndex() };
       })
     };
   });
@@ -55,9 +55,39 @@ describe('Runner#start', () => {
       store,
       taskManager
     }).start();
-    expect(store.getLogger).toMatchSnapshot('store.getLogger');
-    expect(taskManager).toMatchSnapshot('taskManager');
-    expect(taskLogger.log).toMatchSnapshot('taskLogger.log');
-    expect(store.complete).toMatchSnapshot('store.complete');
+    expect(taskManager.mock.calls).toEqual([
+      [{ id: 'task1', callIndex: 0 }],
+      [{ id: 'task2', callIndex: 3 }],
+      [{ id: 'task3', callIndex: 6 }]
+    ]);
+    expect(taskLogger.log.mock.calls).toEqual([
+      [
+        'task1',
+        {
+          task: { id: 'task1', callIndex: 0 },
+          measures: { result: 'result-task1', callIndex: 1 }
+        }
+      ],
+      // call 2 should correspond to the above call to logTask
+      [
+        'task2',
+        {
+          task: { id: 'task2', callIndex: 3 },
+          measures: { result: 'result-task2', callIndex: 4 }
+        }
+      ],
+      // call 5 should correspond to the above call to logTask
+      [
+        'task3',
+        {
+          task: { id: 'task3', callIndex: 6 },
+          measures: { result: 'result-task3', callIndex: 7 }
+        }
+      ]
+      // call 6 should correspond to the above call to logTask
+    ]);
+    expect(store.complete.mock.results[0].value).resolves.toEqual({
+      callIndex: 9
+    });
   });
 });
