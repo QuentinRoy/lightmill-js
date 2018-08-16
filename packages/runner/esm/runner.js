47832c0f5f1f82833ae75fea25dc18986a080b66
Quentin Roy <quentin@quentinroy.fr>
2018-08-16 19:22:25 -0400

    feat(runner): refactor runner to remove babel-runtime dependency

--- packages/runner/esm/runner.js
+++ packages/runner/esm/runner.js
@@ -1,22 +1,14 @@
-import { reduceAsyncIterator } from './utils';
+import { asyncForEach } from './utils';
 
 export default ({ store, taskIterator, runTask }) => ({
   run: () => {
     const taskLogger = store.getLogger('task');
-    return reduceAsyncIterator(
-      taskIterator,
-      async (_, task) => {
-        // Run the task.
-        const measures = await runTask(task);
-        // Log the results.
-        await taskLogger.log(task.id, { task, measures });
-      },
-      null
-    ).then(async () => {
-      // At the very end (when all tasks has been done), we mark the experiment
-      // as complete. Do not directly return store.complete because we don't
-      // want to return the result of the call.
-      await store.complete();
-    });
+    const runThenLog = task =>
+      runTask(task)
+        .then(measures => taskLogger.log(task.id, { task, measures }))
+        .then(() => {});
+    return asyncForEach(taskIterator, runThenLog)
+      .then(() => store.complete())
+      .then(() => {});
   }
 });
--- packages/runner/esm/utils.test.js
+++ packages/runner/esm/utils.test.js
@@ -1,46 +1,104 @@
-import { reduceAsyncIterator } from './utils';
+import { asyncReduce, asyncForEach } from './utils';
 
-const wait = t =>
-  new Promise(resolve => {
-    setTimeout(resolve, t);
+let wait;
+let generators;
+
+beforeEach(() => {
+  wait = t =>
+    new Promise(resolve => {
+      setTimeout(resolve, t);
+    });
+
+  generators = {
+    sync: () => {
+      const arr = ['a', 'b', 'c', 'd'];
+      return arr[Symbol.iterator]();
+    },
+    async: async function* gen() {
+      yield 'a';
+      await wait(0);
+      yield 'b';
+      await wait(0);
+      yield 'c';
+      await wait(0);
+      yield 'd';
+    }
+  };
+});
+
+const describeEachSyncs = describe.each(['sync', 'async']);
+
+describeEachSyncs(`asyncReduce with %s iterator`, sync => {
+  it('reduces without init', async () => {
+    const gen = generators[sync];
+    // Without init.
+    await expect(
+      asyncReduce(gen(), (acc, v, i) => `${acc}${v}${i},`)
+    ).resolves.toBe('ab1,c2,d3,');
   });
 
-test('reduceAsyncIterator', async () => {
-  async function* gen() {
-    yield 1;
-    await wait(0);
-    yield 2;
-    await wait(0);
-    yield 4;
-    await wait(0);
-    yield 9;
-  }
-  // Without init.
-  await expect(reduceAsyncIterator(gen(), (acc, v) => acc + v)).resolves.toBe(
-    16
-  );
-  // With init.
-  await expect(
-    reduceAsyncIterator(gen(), (acc, v, i) => [...acc, { v, i }], [])
-  ).resolves.toEqual([
-    { v: 1, i: 0 },
-    { v: 2, i: 1 },
-    { v: 4, i: 2 },
-    { v: 9, i: 3 }
-  ]);
+  it('reduces with init', async () => {
+    const gen = generators[sync];
+    // Without init.
+    await expect(
+      asyncReduce(gen(), (acc, v, i) => `${acc}${v}${i},`, 'test:')
+    ).resolves.toBe('test:a0,b1,c2,d3,');
+  });
 
-  // With (not always) async transform.
-  await expect(
-    reduceAsyncIterator(
-      gen(),
-      (acc, v, i) =>
-        v % 2 > 0 ? wait(0).then(() => [...acc, { v, i }]) : [...acc, { v, i }],
-      []
-    )
-  ).resolves.toEqual([
-    { v: 1, i: 0 },
-    { v: 2, i: 1 },
-    { v: 4, i: 2 },
-    { v: 9, i: 3 }
-  ]);
+  it('waits for async reducers', async () => {
+    const gen = generators[sync];
+    await expect(
+      asyncReduce(
+        gen(),
+        (acc, v, i) => {
+          const result = `${acc}${v}${i},`;
+          // return asynchronously
+          if (i % 2) return wait(0).then(() => result);
+          // return synchronously
+          return result;
+        },
+        'test:'
+      )
+    ).resolves.toBe('test:a0,b1,c2,d3,');
+  });
+});
+
+describeEachSyncs(`asyncForEach with %s iterator`, sync => {
+  it('calls its callback for each values', async () => {
+    const gen = generators[sync];
+    const callback = jest.fn();
+    await asyncForEach(gen(), callback);
+    expect(callback.mock.calls).toEqual([
+      ['a', 0],
+      ['b', 1],
+      ['c', 2],
+      ['d', 3]
+    ]);
+  });
+  it('waits for its callback to resolve if it returned a promise', async () => {
+    const gen = generators[sync];
+    const acc = [];
+    const asyncCallback = (value, i) => {
+      acc.push(`${value}${i}-async-start`);
+      return wait(0).then(() => {
+        acc.push(`${value}${i}-async-end`);
+      });
+    };
+    const syncCallback = (value, i) => {
+      acc.push(`${value}${i}-sync`);
+    };
+    const callback = jest.fn(
+      (value, i) => (i % 2 ? syncCallback(value, i) : asyncCallback(value, i))
+    );
+
+    await asyncForEach(gen(), callback);
+    expect(acc).toEqual([
+      'a0-async-start',
+      'a0-async-end',
+      'b1-sync',
+      'c2-async-start',
+      'c2-async-end',
+      'd3-sync'
+    ]);
+  });
 });
