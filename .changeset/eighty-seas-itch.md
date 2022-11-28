37f1b0352d5a15c6dc74578b40482797ddaf231b
Quentin Roy <contact@quentinroy.fr>
2022-11-28 16:03:13 +0100

    runner: new Runner main export

---
+++ .changeset/eighty-seas-itch.md
@@ -0,0 +1,5 @@
+---
+'@lightmill/runner': major
+---
+
+New Runner API as default export. The old `run` function is still available as a named export.
---
+++ packages/runner/src/run.ts
@@ -0,0 +1,21 @@
+import { asyncForEach } from './utils.js';
+
+export type SuperIterator<I> =
+  | Iterator<I>
+  | AsyncIterator<I>
+  | Iterable<I>
+  | AsyncIterable<I>;
+
+export type RunProps<Task> = {
+  taskIterator: SuperIterator<Task>;
+  runTask: (task: Task) => PromiseLike<void>;
+};
+export default function run<Task>({ taskIterator, runTask }: RunProps<Task>) {
+  if (Symbol.iterator in taskIterator) {
+    taskIterator = taskIterator[Symbol.iterator]();
+  }
+  if (Symbol.asyncIterator in taskIterator) {
+    taskIterator = taskIterator[Symbol.asyncIterator]();
+  }
+  return asyncForEach(taskIterator, (task) => runTask(task));
+}
