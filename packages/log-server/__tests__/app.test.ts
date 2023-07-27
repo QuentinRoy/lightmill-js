a8a03d3ae750489f43058ab833d90d8ec94a7613
Quentin Roy <contact@quentinroy.fr>
2023-07-27 16:47:13 +0200

    add post /experiments/runs tests

--- packages/log-server/__tests__/app.test.ts
+++ packages/log-server/__tests__/app.test.ts
@@ -1,28 +1,38 @@
 import request from 'supertest';
-import { afterEach, describe, beforeEach, it, vi } from 'vitest';
-import { Store } from '../src/store.js';
+import {
+  afterEach,
+  describe,
+  beforeEach,
+  it,
+  vi,
+  MockedFunction,
+  expect,
+} from 'vitest';
+import { Store, StoreError } from '../src/store.js';
 import { createLogServer } from '../src/app.js';
 
-function MockStore(init: Partial<Store> = {}): Store {
-  return {
-    addRun: vi.fn(() => Promise.reject('not implemented')),
-    getRun: vi.fn(() => Promise.reject('not implemented')),
-    setRunStatus: vi.fn(() => Promise.reject('not implemented')),
-    addRunLogs: vi.fn(() => Promise.reject('not implemented')),
-    getLogValueNames: vi.fn(() => Promise.reject('not implemented')),
+function MockStore<T extends Partial<Store>>(init: T): Store & T;
+function MockStore(): Store;
+function MockStore<T extends Partial<Store>>(init?: T): Store {
+  let store: Store = {
+    addRun: () => Promise.reject('not implemented'),
+    getRun: () => Promise.reject('not implemented'),
+    setRunStatus: () => Promise.reject('not implemented'),
+    addRunLogs: () => Promise.reject('not implemented'),
+    getLogValueNames: () => Promise.reject('not implemented'),
     // eslint-disable-next-line require-yield
-    getLogs: vi.fn(async function* () {
+    getLogs: async function* () {
       throw new Error('not implemented');
-    }),
-    ...init,
+    },
   };
+  return { ...store, ...init };
 }
 
 afterEach(() => {
   vi.resetAllMocks();
 });
 
-describe('sessions', () => {
+describe('/sessions', () => {
   let api: request.SuperTest<request.Test>;
   beforeEach(() => {
     let app = createLogServer({
@@ -118,6 +128,137 @@ describe('sessions', () => {
       await api.post('/sessions').send({ role: 'participant' }).expect(201);
       await api.delete('/sessions/current').expect(200, { status: 'ok' });
       await api.get('/sessions/current').expect(404);
+    });
+  });
+});
+
+describe('/experiments/runs', () => {
+  let store: Store & {
+    addRun: MockedFunction<Store['addRun']>;
+  };
+  let api: request.SuperTest<request.Test>;
+  beforeEach(async () => {
+    vi.useFakeTimers({ toFake: ['Date'] });
+    vi.setSystemTime(1234567890);
+    store = MockStore({
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      addRun: vi.fn(async (_opt) => {
+        return { runId: 'run-id', experimentId: 'exp-id' };
+      }),
+    });
+    let app = createLogServer({
+      store,
+      secret: 'secret',
+      secureCookies: false,
+    });
+    api = request.agent(app);
+    await api.post('/sessions').send({ role: 'participant' });
+  });
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
+  describe('post /experiments/runs', () => {
+    it('should create a run', async () => {
+      await api
+        .post('/experiments/runs')
+        .send({ experiment: 'exp-id', id: 'run-id' })
+        .expect(201, {
+          experiment: 'exp-id',
+          links: {
+            logs: '/experiments/exp-id/runs/run-id/logs',
+            run: '/experiments/exp-id/runs/run-id',
+          },
+          run: 'run-id',
+          status: 'ok',
+        });
+      expect(store.addRun).toHaveBeenCalledWith({
+        experimentId: 'exp-id',
+        runId: 'run-id',
+        createdAt: vi.getMockedSystemTime(),
+      });
+    });
+
+    it('should use the default experiment if the experiment is not named', async () => {
+      await api
+        .post('/experiments/runs')
+        .send({ id: 'run' })
+        .expect(201, {
+          status: 'ok',
+          experiment: 'default',
+          run: 'run',
+          links: {
+            logs: '/experiments/default/runs/run/logs',
+            run: '/experiments/default/runs/run',
+          },
+        });
+      expect(store.addRun).toHaveBeenCalledWith({
+        experimentId: 'default',
+        runId: 'run',
+        createdAt: vi.getMockedSystemTime(),
+      });
+    });
+
+    it('should generate a unique run id if the run id is not provided', async () => {
+      let resp = await api
+        .post('/experiments/runs')
+        .send({ experiment: 'exp' })
+        .expect(201);
+      let runId = resp.body.run;
+      expect(runId).toBeDefined();
+      expect(resp.body).toEqual({
+        status: 'ok',
+        experiment: 'exp',
+        run: runId,
+        links: {
+          logs: `/experiments/exp/runs/${runId}/logs`,
+          run: `/experiments/exp/runs/${runId}`,
+        },
+      });
+      expect(store.addRun).toHaveBeenCalledWith({
+        experimentId: 'exp',
+        runId,
+        createdAt: vi.getMockedSystemTime(),
+      });
+    });
+
+    it('should add the run to the session', async () => {
+      await api
+        .post('/experiments/runs')
+        .send({ experiment: 'exp-id', id: 'run-id' })
+        .expect(201);
+      await api.get('/sessions/current').expect(200, {
+        role: 'participant',
+        runs: [{ id: 'run-id', experiment: 'exp-id' }],
+        status: 'ok',
+      });
+    });
+
+    it('should refuse to create a run if the participant already has one running', async () => {
+      await api
+        .post('/experiments/runs')
+        .send({ experiment: 'exp-id', id: 'run1' })
+        .expect(201);
+      await api
+        .post('/experiments/runs')
+        .send({ experiment: 'exp-id', id: 'run2' })
+        .expect(403, {
+          message: 'Client already has a started run, end it first',
+          status: 'error',
+        });
+    });
+
+    it('should gracefully fail if the run already exists', async () => {
+      store.addRun.mockImplementation(async ({ runId }) => {
+        throw new StoreError(`run "${runId}" already exists`, 'RUN_EXISTS');
+      });
+      await api
+        .post('/experiments/runs')
+        .send({ experiment: 'exp-id', id: 'run-id' })
+        .expect(400, {
+          status: 'error',
+          message: 'run "run-id" already exists',
+        });
     });
   });
 });
