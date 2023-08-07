e18c6d102ebce5d35db8207b5ba72f640c11533a
Quentin Roy <contact@quentinroy.fr>
2023-08-07 12:05:52 +0200

    fix log server build

--- packages/log-server/src/index.ts
+++ packages/log-server/src/index.ts
@@ -1,12 +1,2 @@
 export { Store, SQLiteStore } from './store.js';
 export { LogServer } from './app.js';
-export type {
-  Path,
-  Method,
-  PathParams,
-  QueryParams,
-  Body,
-  Response,
-  Error,
-  ErrorStatus,
-} from './api.js';
