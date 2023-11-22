f98c61caa89ff0ed1da2ae215ed3f2b867afa1b4
Quentin Roy <contact@quentinroy.fr>
2023-11-22 10:32:45 +0100

    setup publint and package.json fixes
    fixes #130

---
+++ scripts/publint.js
@@ -0,0 +1,52 @@
+import { readdir } from 'node:fs/promises';
+import { spawn } from 'node:child_process';
+import { parseArgs } from 'node:util';
+
+function publint(path) {
+  return new Promise((resolve, reject) => {
+    const child = spawn('publint', [path]);
+    child.stdout.on('data', (data) => {
+      console.log(data.toString());
+    });
+
+    child.stderr.on('data', (data) => {
+      console.error(data.toString());
+    });
+
+    child.on('close', (code) => {
+      if (code !== 0) {
+        reject(code);
+      } else {
+        resolve(code);
+      }
+    });
+  });
+}
+
+async function publintAll() {
+  let hasError = false;
+  await publint('./').catch(() => {
+    hasError = true;
+  });
+  for (const packagePath of await readdir('./packages')) {
+    await publint(`./packages/${packagePath}`).catch(() => {
+      hasError = true;
+    });
+  }
+
+  if (hasError) {
+    throw new Error('publint failed');
+  }
+}
+
+const [path] = parseArgs({ allowPositionals: true }).positionals;
+
+if (path == null) {
+  publintAll().catch(() => {
+    process.exit(1);
+  });
+} else {
+  publint(path).catch(() => {
+    process.exit(1);
+  });
+}
