669e42467f016ced0df656542e89aff3c6eeedf4
Quentin Roy <quentin@quentinroy.fr>
2023-11-27 15:16:03 +0100

    react-experiment: prevent importing private files

--- packages/react-experiment/src/contexts.ts
+++ packages/react-experiment/src/contexts.ts
@@ -4,6 +4,7 @@ import { TimelineState } from './useManagedTimeline.js';
 
 export const timelineContext =
   React.createContext<TimelineState<RegisteredTask> | null>(null);
+
 export const loggerContext = React.createContext<
   ((log: RegisteredLog) => void) | null
 >(null);
--- packages/react-experiment/src/main.tsx
+++ packages/react-experiment/src/main.tsx
@@ -1,4 +1,4 @@
-export { Run, RunElements } from './run.js';
-export { RegisterExperiment } from './config.js';
+export { Run, type RunElements } from './run.js';
+export { type RegisterExperiment } from './config.js';
 export { useLogger } from './useLogger.js';
 export { useTask } from './useTask.js';
