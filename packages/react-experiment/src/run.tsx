QuentinRoy/lightmill-js

commit 6ee33d3d21882c2e6d8cc12ec0cfda5506fce46a

react-experiment: improve run typing (#211)
* react-experiment: improve run typing

* changeset

packages/react-experiment/src/run.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { ReadonlyDeep } from 'type-fest';
 import { RegisteredTask, Typed, RegisteredLog } from './config.js';
 import { loggerContext, noLoggerSymbol, timelineContext } from './contexts.js';
 import useManagedTimeline, {
@@ -15,13 +16,13 @@ export type RunElements<T extends Typed> = {
 export type Logger<Log> = (log: Log) => Promise<void>;
 
 export type RunProps<Task extends Typed, Log> = {
-  elements: RunElements<Task>;
+  elements: Readonly<RunElements<Task>>;
   confirmBeforeUnload?: boolean;
 } & UseRunParameter<Task, Log>;
 
 // This component uses explicit return type to prevent the function from
 // returning undefined, which could indicate a state isn't being handled.
-export function Run<T extends RegisteredTask>({
+export function Run<const T extends RegisteredTask>({
   elements,
   confirmBeforeUnload = true,
   ...useRunParameter
@@ -74,13 +75,13 @@ type UseRunParameter<Task extends { type: string }, Log> = {
   onLog?: Logger<Log>;
   resumeAfter?: { type: Task['type']; number: number };
 } & (
-  | { timeline: Timeline<Task>; loading?: boolean }
-  | { timeline?: Timeline<Task> | null; loading: true }
+  | { timeline: ReadonlyDeep<Timeline<Task>>; loading?: boolean }
+  | { timeline?: ReadonlyDeep<Timeline<Task>> | null; loading: true }
 );
 type RunState<Task, Log> = Exclude<TimelineState<Task>, { status: 'error' }> & {
   onLog: ((newLog: Log) => void) | null;
 };
-function useRun<T extends { type: string }, L>({
+function useRun<const T extends { type: string }, L>({
   onCompleted,
   timeline,
   resumeAfter,

commit 938e886533bb85727c027a1618a237984d18e97d

react-experiment: fix type regression from 6ee33d3

packages/react-experiment/src/run.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
-import { ReadonlyDeep } from 'type-fest';
 import { RegisteredTask, Typed, RegisteredLog } from './config.js';
 import { loggerContext, noLoggerSymbol, timelineContext } from './contexts.js';
 import useManagedTimeline, {
-  Timeline,
+  AnyIteratorOrIterable,
   TimelineState,
 } from './useManagedTimeline.js';
 
@@ -16,7 +15,7 @@ export type RunElements<T extends Typed> = {
 export type Logger<Log> = (log: Log) => Promise<void>;
 
 export type RunProps<Task extends Typed, Log> = {
-  elements: Readonly<RunElements<Task>>;
+  elements: RunElements<Task>;
   confirmBeforeUnload?: boolean;
 } & UseRunParameter<Task, Log>;
 
@@ -75,13 +74,13 @@ type UseRunParameter<Task extends { type: string }, Log> = {
   onLog?: Logger<Log>;
   resumeAfter?: { type: Task['type']; number: number };
 } & (
-  | { timeline: ReadonlyDeep<Timeline<Task>>; loading?: boolean }
-  | { timeline?: ReadonlyDeep<Timeline<Task>> | null; loading: true }
+  | { timeline: AnyIteratorOrIterable<Task>; loading?: boolean }
+  | { timeline?: AnyIteratorOrIterable<Task> | null; loading: true }
 );
 type RunState<Task, Log> = Exclude<TimelineState<Task>, { status: 'error' }> & {
   onLog: ((newLog: Log) => void) | null;
 };
-function useRun<const T extends { type: string }, L>({
+function useRun<T extends { type: string }, L>({
   onCompleted,
   timeline,
   resumeAfter,

packages/react-experiment/src/useManagedTimeline.ts
@@ -11,11 +11,11 @@ export type TimelineState<Task> =
   | { status: 'error'; error: Error }
   | { status: 'running'; task: Task; onTaskCompleted: () => void };
 
-export type AsyncTimeline<Task> = AsyncIterator<Task> | AsyncIterable<Task>;
-
-export type SyncTimeline<Task> = Iterator<Task> | Iterable<Task>;
-
-export type Timeline<Task> = AsyncTimeline<Task> | SyncTimeline<Task>;
+export type AnyIteratorOrIterable<Task> =
+  | AsyncIterator<Task>
+  | AsyncIterable<Task>
+  | Iterator<Task>
+  | Iterable<Task>;
 
 type Options<Task extends { type: string }> = {
   onTimelineCompleted?: () => void;
@@ -23,7 +23,7 @@ type Options<Task extends { type: string }> = {
   onTaskStarted?: (task: Task) => void;
   onTaskCompleted?: (task: Task) => void;
   onTaskLoadingError?: (error: unknown) => void;
-  timeline?: Timeline<Task> | null;
+  timeline?: AnyIteratorOrIterable<Task> | null;
   resumeAfter?: { type: Task['type']; number: number };
 };
 