9e432a09b8bacd9019982d5643e4df014ba5bb90
Quentin Roy <quentin@quentinroy.fr>
2024-01-25 16:25:53 +0100

    update

--- packages/log-server/vitest.config.ts
+++ packages/log-server/vitest.config.ts
@@ -1,14 +1,14 @@
 import { resolve } from 'node:path';
 import * as url from 'node:url';
-import { defineConfig } from 'vitest/config';
+import {  defineProject } from 'vitest/config';
 
 const dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
-export default defineConfig({
+export default defineProject({
   test: {
-    root: resolve(dirname),
-    globals: false,
+    environment: 'node',
     include: ['**/__tests__/*.test.ts'],
+    root: resolve(dirname),
     // This are the default values with dist omitted because we do run some of
     // the tests against the dist build.
     watchExclude: ['**/node_modules/**'],
