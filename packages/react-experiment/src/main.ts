fe2e5dcbeec55965ab0bf558a3eae9875c18222d
Quentin Roy <quentin@quentinroy.fr>
2025-05-20 15:39:34 +0200

    react-experiment: rename main.tsx to main.ts

---
+++ packages/react-experiment/src/main.ts
@@ -0,0 +1,4 @@
+export { type RegisterExperiment } from './config.ts';
+export { Run, type RunElements } from './run.tsx';
+export { useLogger } from './useLogger.ts';
+export { useTask } from './useTask.ts';
--- packages/react-experiment/src/main.tsx
+++
@@ -1,4 +0,0 @@
-export { type RegisterExperiment } from './config.js';
-export { Run, type RunElements } from './run.js';
-export { useLogger } from './useLogger.js';
-export { useTask } from './useTask.js';
